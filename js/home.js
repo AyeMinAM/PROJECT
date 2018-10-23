fc4e6cac595672b9c51549e86efe4375cd311690
AyeMin <ayemin@kootour.com>
2018-10-23 16:42:57 -0700

    nav overlay

---
+++ js/home.js
@@ -0,0 +1,8 @@
+
+      function openNav() {
+          document.getElementById("navbarMobile").style.height = "100%";
+      }
+
+      function closeNav() {
+          document.getElementById("navbarMobile").style.height = "0%";
+      }
